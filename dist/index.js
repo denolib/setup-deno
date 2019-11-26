3b81720efca7f8c45089473ec4b284429d6f30e5
木杉 <zhmushan@qq.com>
2019-11-26 13:01:03 +0800

    test ci

--- dist/index.js
+++ dist/index.js
@@ -928,6 +928,122 @@ module.exports = require("tls");
 
 /***/ }),
 
+/***/ 86:
+/***/ (function(module, __unusedexports, __webpack_require__) {
+
+var rng = __webpack_require__(139);
+var bytesToUuid = __webpack_require__(722);
+
+// **`v1()` - Generate time-based UUID**
+//
+// Inspired by https://github.com/LiosK/UUID.js
+// and http://docs.python.org/library/uuid.html
+
+var _nodeId;
+var _clockseq;
+
+// Previous uuid creation time
+var _lastMSecs = 0;
+var _lastNSecs = 0;
+
+// See https://github.com/broofa/node-uuid for API details
+function v1(options, buf, offset) {
+  var i = buf && offset || 0;
+  var b = buf || [];
+
+  options = options || {};
+  var node = options.node || _nodeId;
+  var clockseq = options.clockseq !== undefined ? options.clockseq : _clockseq;
+
+  // node and clockseq need to be initialized to random values if they're not
+  // specified.  We do this lazily to minimize issues related to insufficient
+  // system entropy.  See #189
+  if (node == null || clockseq == null) {
+    var seedBytes = rng();
+    if (node == null) {
+      // Per 4.5, create and 48-bit node id, (47 random bits + multicast bit = 1)
+      node = _nodeId = [
+        seedBytes[0] | 0x01,
+        seedBytes[1], seedBytes[2], seedBytes[3], seedBytes[4], seedBytes[5]
+      ];
+    }
+    if (clockseq == null) {
+      // Per 4.2.2, randomize (14 bit) clockseq
+      clockseq = _clockseq = (seedBytes[6] << 8 | seedBytes[7]) & 0x3fff;
+    }
+  }
+
+  // UUID timestamps are 100 nano-second units since the Gregorian epoch,
+  // (1582-10-15 00:00).  JSNumbers aren't precise enough for this, so
+  // time is handled internally as 'msecs' (integer milliseconds) and 'nsecs'
+  // (100-nanoseconds offset from msecs) since unix epoch, 1970-01-01 00:00.
+  var msecs = options.msecs !== undefined ? options.msecs : new Date().getTime();
+
+  // Per 4.2.1.2, use count of uuid's generated during the current clock
+  // cycle to simulate higher resolution clock
+  var nsecs = options.nsecs !== undefined ? options.nsecs : _lastNSecs + 1;
+
+  // Time since last uuid creation (in msecs)
+  var dt = (msecs - _lastMSecs) + (nsecs - _lastNSecs)/10000;
+
+  // Per 4.2.1.2, Bump clockseq on clock regression
+  if (dt < 0 && options.clockseq === undefined) {
+    clockseq = clockseq + 1 & 0x3fff;
+  }
+
+  // Reset nsecs if clock regresses (new clockseq) or we've moved onto a new
+  // time interval
+  if ((dt < 0 || msecs > _lastMSecs) && options.nsecs === undefined) {
+    nsecs = 0;
+  }
+
+  // Per 4.2.1.2 Throw error if too many uuids are requested
+  if (nsecs >= 10000) {
+    throw new Error('uuid.v1(): Can\'t create more than 10M uuids/sec');
+  }
+
+  _lastMSecs = msecs;
+  _lastNSecs = nsecs;
+  _clockseq = clockseq;
+
+  // Per 4.1.4 - Convert from unix epoch to Gregorian epoch
+  msecs += 12219292800000;
+
+  // `time_low`
+  var tl = ((msecs & 0xfffffff) * 10000 + nsecs) % 0x100000000;
+  b[i++] = tl >>> 24 & 0xff;
+  b[i++] = tl >>> 16 & 0xff;
+  b[i++] = tl >>> 8 & 0xff;
+  b[i++] = tl & 0xff;
+
+  // `time_mid`
+  var tmh = (msecs / 0x100000000 * 10000) & 0xfffffff;
+  b[i++] = tmh >>> 8 & 0xff;
+  b[i++] = tmh & 0xff;
+
+  // `time_high_and_version`
+  b[i++] = tmh >>> 24 & 0xf | 0x10; // include version
+  b[i++] = tmh >>> 16 & 0xff;
+
+  // `clock_seq_hi_and_reserved` (Per 4.2.2 - include variant)
+  b[i++] = clockseq >>> 8 | 0x80;
+
+  // `clock_seq_low`
+  b[i++] = clockseq & 0xff;
+
+  // `node`
+  for (var n = 0; n < 6; ++n) {
+    b[i + n] = node[n];
+  }
+
+  return buf ? buf : bytesToUuid(b);
+}
+
+module.exports = v1;
+
+
+/***/ }),
+
 /***/ 87:
 /***/ (function(module) {
 
@@ -1188,7 +1304,6 @@ function run() {
             if (!version) {
                 version = core.getInput("deno-version");
             }
-            core.debug(`Installing deno at version ${version}`);
             if (version) {
                 // TODO: installer doesn't support proxy
                 yield installer.getDeno(version);
@@ -4674,6 +4789,21 @@ exports.HttpClient = HttpClient;
 
 /***/ }),
 
+/***/ 898:
+/***/ (function(module, __unusedexports, __webpack_require__) {
+
+var v1 = __webpack_require__(86);
+var v4 = __webpack_require__(826);
+
+var uuid = v4;
+uuid.v1 = v1;
+uuid.v4 = v4;
+
+module.exports = uuid;
+
+
+/***/ }),
+
 /***/ 923:
 /***/ (function(__unusedmodule, exports, __webpack_require__) {
 
@@ -4699,6 +4829,7 @@ const tc = __webpack_require__(533);
 const exec = __webpack_require__(986);
 const io = __webpack_require__(1);
 const restm = __webpack_require__(105);
+const uuidV4 = __webpack_require__(898);
 if (!tempDirectory) {
     let baseLocation;
     if (process.platform == "win32") {
@@ -4715,7 +4846,9 @@ if (!tempDirectory) {
     }
     tempDirectory = path.join(baseLocation, "actions", "cache");
 }
-const osArch = os.arch();
+function osArch() {
+    return "x64";
+}
 function osPlat() {
     const platform = os.platform();
     let rtv = null;
@@ -4731,23 +4864,21 @@ function osPlat() {
 }
 function getDeno(version) {
     return __awaiter(this, void 0, void 0, function* () {
-        version = yield clearVersion(version);
-        // check cache
         let toolPath;
-        toolPath = tc.find("deno", version);
-        // If not found in cache, download
-        if (!toolPath) {
+        walk: {
+            // check cache
+            toolPath = tc.find("deno", version);
+            if (toolPath)
+                break walk;
+            version = yield clearVersion(version);
+            // check cache
+            toolPath = tc.find("deno", version);
+            if (toolPath)
+                break walk;
+            // If not found in cache, download
+            core.debug(`Downloading deno at version ${version}`);
             toolPath = yield acquireDeno(version);
         }
-        //
-        // a tool installer initimately knows details about the layout of that tool
-        // for example, deno binary is in the bin folder after the extract on Mac/Linux.
-        // layouts could change by version, by platform etc... but that's the tool installers job
-        //
-        if (osPlat() != "win") {
-            toolPath = path.join(toolPath, "bin");
-        }
-        //
         // prepend the tools path. instructs the agent to prepend for future tasks
         core.addPath(toolPath);
     });
@@ -4769,6 +4900,7 @@ function clearVersion(version) {
         return version;
     });
 }
+exports.clearVersion = clearVersion;
 function queryLatestMatch(versionSpec) {
     return __awaiter(this, void 0, void 0, function* () {
         function cmp(a, b) {
@@ -4812,11 +4944,10 @@ function acquireDeno(version) {
         else {
             throw new Error(`Unable to find Deno version ${version}`);
         }
-        const fileName = `deno_${osPlat()}_${osArch}`;
+        const fileName = `deno_${osPlat()}_${osArch()}`;
         const urlFileName = osPlat() == "win" ? `${fileName}.zip` : `${fileName}.gz`;
         const downloadUrl = `https://github.com/denoland/deno/releases/download/v${version}/${urlFileName}`;
-        let downloadPath;
-        downloadPath = yield tc.downloadTool(downloadUrl);
+        let downloadPath = yield tc.downloadTool(downloadUrl);
         //
         // Extract
         //
@@ -4825,16 +4956,19 @@ function acquireDeno(version) {
             extPath = yield tc.extractZip(downloadPath);
         }
         else {
-            const gzip = yield io.which("gzip");
-            yield exec.exec(gzip, ["-dk", downloadPath]);
+            extPath = path.join(downloadPath, "..", uuidV4());
+            const gzFile = path.join(extPath, "deno.gz");
+            yield io.mv(downloadPath, gzFile);
+            const gzPzth = yield io.which("gzip");
+            yield exec.exec(gzPzth, ["-d", gzFile]);
         }
         //
         // Install into the local tool cache - deno extracts a file that matches the fileName downloaded
         //
-        let toolRoot = path.join(extPath, fileName);
-        return yield tc.cacheDir(toolRoot, "deno", version);
+        return yield tc.cacheDir(extPath, "deno", version);
     });
 }
+exports.acquireDeno = acquireDeno;
 
 
 /***/ }),
